afb687b50c9641df2a9d0116a75c94c1b347dade
U-Hyun-98 <uhyun1115@gmail.com>
2023-04-19 04:14:24 +0900

    :ambulance: Fix : 풀 리퀘 에러 테스트2

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import Header from "./component/Header";
+import Contnent from './component/Content';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Header />
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Contnent />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
