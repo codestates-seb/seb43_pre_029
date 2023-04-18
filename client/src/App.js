49f70e9592ab93990a8eb85924881ab6f483b543
U-Hyun-98 <uhyun1115@gmail.com>
2023-04-19 03:24:53 +0900

    :ambulance: Fix : 풀리퀘 에러 테스트

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-// import Header from "./component/Header";
-import Footer from "./component/Footer";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
-      <Footer />
+      <Header />
     
     </div>
   );
