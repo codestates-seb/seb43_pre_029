9ebffec3a1bf82dcfef0c693ebb56e0faecf8b63
dongwook98 <ehddnr8563@gmail.com>
2023-04-19 11:52:56 +0900

    🎨 Style : Home 페이지 헤더 공간

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-import homeimg from '../image/home_img.png';
+import homeimg from '../assets/home_img.png';
 
 const Imgstyle = styled.div`
   img {
+    padding-top: 2.8125rem;
     width: 100%;
   }
 `;
 
 export default function Home() {
   return (
-    <div>
-      <Imgstyle>
-        <img src={homeimg} alt="" />
-      </Imgstyle>
-    </div>
+    <Imgstyle>
+      <img src={homeimg} alt="" />
+    </Imgstyle>
   );
 }
