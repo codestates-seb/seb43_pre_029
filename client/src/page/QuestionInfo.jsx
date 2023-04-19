47572e950409b15609eea4ac32043a37f1eeff74
dohyun98 <kch9554@naver.com>
2023-04-19 12:46:56 +0900

    :sparkles: Feat : page/QuestionInfo

--- client/src/page/QuestionInfo.jsx
+++ client/src/page/QuestionInfo.jsx
@@ -54,6 +54,62 @@ const InfoheaderUnder = styled.div`
   }
 `;
 
+const InfoMain = styled.div`
+  padding: 20px 25px;
+  display: flex;
+  border: 1px solid red;
+  height: auto;
+`;
+const InfoSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid yellow;
+  margin-right: 16px;
+  button {
+    width: 35px;
+    height: 35px;
+    border-style: none;
+    background-color: white;
+  }
+`;
+const InfoValue = styled.div`
+  border: 1px solid blue;
+  width: 100vw;
+  margin-right: 16px;
+
+  .infoValue {
+    border: 1px solid;
+    margin-bottom: 16.5px;
+  }
+`;
+const InfoValueUnder = styled.div`
+  display: flex;
+  justify-content: end;
+  .userProfil {
+    border: 1px solid tomato;
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    height: 67px;
+    padding: 5px 6px 7px 7px;
+    margin-left: 80px;
+    .creatTime {
+      width: 150px;
+      margin-bottom: 3px;
+    }
+    .user {
+      display: flex;
+      width: 150px;
+      border: 1px solid blue;
+      .name {
+        border: 1px solid gold;
+        margin-left: 7px;
+      }
+    }
+  }
+`;
+
 const QuestionInfo = () => {
   return (
     <>
@@ -68,14 +124,29 @@ const QuestionInfo = () => {
         <span>Modified today</span>
         <span>Viewed 18 times</span>
       </InfoheaderUnder>
-      <div className="InfoMain">
-        <div className="InfoSide">추천 버튼</div>
-        <div className="InfoValue">
-          <div className="Improve">Closed, 하늘색 박스</div>
-          <div className="">value</div>
-          <div className="userProfil">유저 프로필</div>
-        </div>
-      </div>
+      <InfoMain>
+        <InfoSide>
+          <button className="upBtn">Up</button>0<button className="downBtn">Down</button>
+          <button>북마크</button>
+          <button>아이콘</button>
+        </InfoSide>
+        <InfoValue>
+          <div className="infoValue">value</div>
+          <InfoValueUnder>
+            <div>edited 2 hours ago</div>
+            <div className="userProfil">
+              <div className="creatTime">
+                <span>asked </span>
+                <span>43 secs ago</span>
+              </div>
+              <div className="user">
+                <div>프로필 이미지</div>
+                <div className="name">이름</div>
+              </div>
+            </div>
+          </InfoValueUnder>
+        </InfoValue>
+      </InfoMain>
     </>
   );
 };
