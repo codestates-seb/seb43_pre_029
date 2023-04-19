8ad3043af78e7dcd6fb6828f85a02c507245b04f
Kadesti <adesti3527@gmail.com>
2023-04-19 09:19:33 +0900

    🔨 Refactor : className="App"

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Contnent from './component/Content';
 
-import styled from 'styled-components';
 import Login from './page/Login'
 import SignUp from './page/SignUp'
 import MyPage from './page/MyPage'
 
+// import styled from 'styled-components';
 /** 설명: FlexBox 센터로 안가서 임의로 만든 스타일 컴포넌트 */
-const AppSample = styled.div`
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  background: #F1F2F3;
+// const AppSample = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-content: center;
+//   background: #F1F2F3;
 
-  height: 100vh;
-`
+//   height: 100vh;
+// `
 
 const App = () => {
   return (
-    // <div className="App">
-    <AppSample>
+    <div className="App">
+      {/* <AppSample> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Contnent />} />
@@ -28,12 +28,9 @@ const App = () => {
           <Route path="/mypage" element={<MyPage />} />
         </Routes>
       </BrowserRouter>
-    </AppSample>
-
+      {/* </AppSample> */}
+    </div>
   );
 };
 
 export default App;
-
-
-
