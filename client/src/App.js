2a2fb71f5580cbc77f623e154b9742dcfb694be9
Kadesti <adesti3527@gmail.com>
2023-04-19 09:53:07 +0900

    ✨ Feat : 라우팅 추가 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,35 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Contnent from './component/Content';
+// import Contnent from './component/Content';
 
 import Login from './page/Login'
 import SignUp from './page/SignUp'
 import MyPage from './page/MyPage'
 
-// import styled from 'styled-components';
+import styled from 'styled-components';
 /** 설명: FlexBox 센터로 안가서 임의로 만든 스타일 컴포넌트 */
-// const AppSample = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-content: center;
-//   background: #F1F2F3;
+const AppSample = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  background: #F1F2F3;
 
-//   height: 100vh;
-// `
+  height: 100vh;
+`
 
 const App = () => {
   return (
     <div className="App">
-      {/* <AppSample> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Contnent />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/mypage" element={<MyPage />} />
-        </Routes>
-      </BrowserRouter>
-      {/* </AppSample> */}
+      <AppSample>
+        <BrowserRouter>
+          <Routes>
+            {/* <Route path="/" element={<Contnent />} /> */}
+            <Route path="/" element={<p>1234</p>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/mypage" element={<MyPage />} />
+          </Routes>
+        </BrowserRouter>
+      </AppSample>
     </div>
   );
 };
