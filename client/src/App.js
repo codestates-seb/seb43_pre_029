275ca182636946b1f8a2a182b92b2a33393f4b35
dongwook98 <ehddnr8563@gmail.com>
2023-04-20 14:54:11 +0900

    ✨ Feat : Ask 페이지 라우팅

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import MyPage from './pages/MyPage'
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import MyPage from './pages/MyPage';
 import Home from './pages/Home';
 import QuestionInfo from './pages/QuestionInfo';
+import Ask from './pages/Ask';
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/mypage" element={<MyPage />} />
-          <Route path="/question/:id" element={<QuestionInfo />} />
+          <Route path="/questions/:id" element={<QuestionInfo />} />
+          <Route path="/questions/ask" element={<Ask />} />
         </Routes>
       </BrowserRouter>
     </div>
