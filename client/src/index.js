c8fe5eb4821ea20ce72da5093036a2c9b243f73a
dongwook98 <ehddnr8563@gmail.com>
2023-04-20 14:55:10 +0900

    🗑️ Remove : styled-reset 사용 해제

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Reset } from 'styled-reset'
-
+import { Reset } from 'styled-reset';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <>
-    <Reset />
+    {/* <Reset /> */}
     <App />,
-  </>
+  </>,
   // </React.StrictMode>
 );
