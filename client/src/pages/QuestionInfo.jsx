939a48c05ab6f72613e5b5cc0a30a09ebc67c7e4
dohyun98 <kch9554@naver.com>
2023-04-21 09:05:23 +0900

    ✨ Feat : Quill 추가

--- client/src/pages/QuestionInfo.jsx
+++ client/src/pages/QuestionInfo.jsx
@@ -1,37 +1,69 @@
 import React, { useEffect, useState } from 'react';
-import Header from '../components/Header';
 import QInfoHeader from '../components/QInfo/QInfoHeader';
-import Footer from '../components/Footer';
 import QInfoMain from '../components/QInfo/QInfoMain';
 import Answer from '../components/QInfo/Answer';
 import axios from 'axios';
 import styled from 'styled-components';
+import NoSideHeader from '../components/Header/NoSideHeader';
+import Comment from '../components/QInfo/Comment';
+import Sidebar from '../components/SideBar';
+import Quill from '../components/Quill';
 
+const QMain = styled.div`
+  width: 800px;
+  margin: auto;
+  margin-top: 2.8125rem;
+`;
 const AnswerTotal = styled.h2`
-  margin-bottom: 8px;
-  margin-left: 16px;
+  margin: 20px 0px 8px 26px;
   font-size: 23px;
 `;
+
+const AnswerInput = styled.div`
+  margin: 16px;
+  h2 {
+    margin-bottom: 16px;
+    padding-top: 20px;
+  }
+`;
+const CommentList = styled.div`
+  border-top: 1px solid rgb(227 230 232);
+  width: auto;
+  margin: 0px 20px 5px 70px;
+`;
+
 const QuestionInfo = () => {
   const [qinfo, setQinfo] = useState([]);
   const [qanswers, setQianswers] = useState([]);
+  const [comments, setComments] = useState([]);
   useEffect(() => {
     axios.get('http://localhost:4000/questions/1').then((res) => {
       setQinfo(res.data);
       setQianswers(res.data.answers);
+      setComments(res.data.comment);
     });
   }, []);
-  console.log(qanswers);
   return (
     <>
-      <Header />
-      <QInfoHeader qinfo={qinfo} />
-      <QInfoMain qinfo={qinfo} />
-      <AnswerTotal>{qanswers.length} Answers</AnswerTotal>
-      {qanswers.map((answer) => (
-        <Answer answer={answer} key={answer.id} />
-      ))}
-      <Footer />
+      <NoSideHeader />
+      <Sidebar />
+      <QMain>
+        <QInfoHeader qinfo={qinfo} />
+        <QInfoMain qinfo={qinfo} />
+        <CommentList>
+          {comments.map((comment) => (
+            <Comment comment={comment} key={comment.id} />
+          ))}
+        </CommentList>
+        <AnswerTotal>{qanswers.length} Answers</AnswerTotal>
+        {qanswers.map((answer) => (
+          <Answer answer={answer} key={answer.id} />
+        ))}
+        <AnswerInput>
+          <h2>Your Answer</h2>
+          <Quill />
+        </AnswerInput>
+      </QMain>
     </>
   );
 };
