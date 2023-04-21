95472d4505b13743b8514482d56031e48067c9bc
dohyun98 <kch9554@naver.com>
2023-04-21 09:13:52 +0900

    🔨 Refactor : import 수정, 컴포넌터 제거

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import MyPage from './pages/MyPage'
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import MyPage from './pages/MyPage';
 import QuestionInfo from './pages/QuestionInfo';
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
