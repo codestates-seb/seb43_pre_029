c89a434402230ec591cb68826d690520db0e16cb
tkfkdgowksel <tkfkdgowksel@gmail.com>
2023-04-21 14:37:28 +0900

    ✨ Feat : Impl 부분 추가

---
+++ server/src/main/generated/com/codestates/comment/mapper/CommentMapperImpl.java
@@ -0,0 +1,13 @@
+package com.codestates.comment.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-04-19T01:31:13+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.18 (Azul Systems, Inc.)"
+)
+@Component
+public class CommentMapperImpl implements CommentMapper {
+}
---
+++ server/src/main/generated/com/codestates/member/mapper/MemberMapperImpl.java
@@ -0,0 +1,51 @@
+package com.codestates.member.mapper;
+
+import com.codestates.member.dto.MemberDto.MemberResponse;
+import com.codestates.member.dto.MemberDto.Post;
+import com.codestates.member.entity.Member;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-04-19T11:27:00+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.18 (Azul Systems, Inc.)"
+)
+@Component
+public class MemberMapperImpl implements MemberMapper {
+
+    @Override
+    public Member MemberPostDtoToMember(Post memberPostDto) {
+        if ( memberPostDto == null ) {
+            return null;
+        }
+
+        Member member = new Member();
+
+        member.setName( memberPostDto.getName() );
+        member.setPassword( memberPostDto.getPassword() );
+        member.setEmail( memberPostDto.getEmail() );
+        member.setAddress( memberPostDto.getAddress() );
+        member.setStatus_message( memberPostDto.getStatus_message() );
+        member.setPhone( memberPostDto.getPhone() );
+
+        return member;
+    }
+
+    @Override
+    public MemberResponse MemberToMemberResponseDto(Member member) {
+        if ( member == null ) {
+            return null;
+        }
+
+        MemberResponse memberResponse = new MemberResponse();
+
+        memberResponse.setEmail( member.getEmail() );
+        memberResponse.setName( member.getName() );
+        memberResponse.setAddress( member.getAddress() );
+        memberResponse.setStatus_message( member.getStatus_message() );
+        memberResponse.setPhone( member.getPhone() );
+
+        return memberResponse;
+    }
+}
