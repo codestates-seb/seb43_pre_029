1100bcab6b86a898b6a223057fa87917ef409a04
Kadesti <adesti3527@gmail.com>
2023-04-23 22:19:13 +0900

    🔨 Refactor : Footer 추가

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import MyPage from './pages/MyPage';
 import QuestionInfo from './pages/QuestionInfo';
-import Footer from './components/Footer';
 import { useState } from 'react';
 
 
@@ -15,13 +14,12 @@ const App = () => {
    * Home이나 mypage question 등에서 logout 버튼 클릭시
    * localStorage.clear() 
    */
-  const [isLogin, setIsLogin] = useState(localStorage.getItem("accessToken"))
+  const [isLogin, setIsLogin] = useState(localStorage.getItem("access_token"))
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* Home > QuestionsList get 요청 못받음 */}
           <Route path="/" element={<Home {...isLogin} />} />
           <Route path="/login" element={isLogin ? <Navigate to="/" /> : <Login />} />
           <Route path="/signup" element={isLogin ? <Navigate to="/" /> : <SignUp />} />
@@ -29,7 +27,6 @@ const App = () => {
           <Route path="/question/:id" element={<QuestionInfo {...isLogin} />} />
         </Routes>
       </BrowserRouter>
-      <Footer />
     </div>
   );
 };
--- client/src/pages/QuestionInfo.jsx
+++ client/src/pages/QuestionInfo.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import FixSideHeader from '.././components/Header/FixSideHeader';
+import FixSideHeader from '../components/Header/Fix/FixSideHeader';
 import QInfoHeader from '../components/QInfo/QInfoHeader';
 import QInfoMain from '../components/QInfo/QInfoMain';
 import Answer from '../components/QInfo/Answer';
@@ -7,6 +7,7 @@ import axios from 'axios';
 import styled from 'styled-components';
 import Comment from '../components/QInfo/Comment';
 import Quill from '../components/Quill';
+import Footer from '../components/Footer/Footer';
 
 const QMain = styled.div`
   width: 800px;
@@ -62,6 +63,7 @@ const QuestionInfo = ({ isLogin }) => {
           <Quill />
         </AnswerInput>
       </QMain>
+      <Footer />
     </>
   );
 };
