aa070d2b48dff57416a210d4b6489cb6f8e12295
dongwook98 <ehddnr8563@gmail.com>
2023-04-23 22:06:01 +0900

    ✨ Feat : 질문 글 등록 기능, 초기화 기능 구현

--- client/src/components/Ask/AskMain.jsx
+++ client/src/components/Ask/AskMain.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import Editor from './Editor';
@@ -5,14 +6,51 @@ import InputTitle from './InputTitle';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { setAskContent, setAskTitle } from '../../store';
 
+/**
+ *
+ * @returns 질문글 작성하는 페이지
+ */
 const AskMain = () => {
   const [inputValue, setInputValue] = useState({
     title: '',
     body: '',
   });
 
+  /**
+   * 질문 글 등록 기능!
+   * @param {*} e : 이벤트 객체
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:4000/questions', inputValue)
+      .then((res) => {
+        alert('질문 게시글 등록 완료하였습니다!');
+        setInputValue((prevInputValue) => ({
+          ...prevInputValue,
+          title: '',
+          body: '',
+        }));
+      })
+      .catch((err) => {
+        console.error(err);
+        alert('질문 게시글 등록에 실패하였습니다.');
+      });
+  };
+
+  /**
+   * 질문 글 초기화 기능!
+   */
+  const handleAllClear = () => {
+    setInputValue((prevInputValue) => ({
+      ...prevInputValue,
+      title: '',
+      body: '',
+    }));
+  };
+
   return (
-    <>
+    <main>
       <form>
         <InputTitle setInputValue={setInputValue} inputValue={inputValue} />
         <Box>
@@ -20,40 +58,29 @@ const AskMain = () => {
           <p>Be specific and imagine you’re asking a question to another person.</p>
           <Editor setInputValue={setInputValue} inputValue={inputValue} />
         </Box>
+        <Flex direction="row">
+          <Btn color="red" type="button" onClick={handleAllClear}>
+            clear(초기화)
+          </Btn>
+          <Btn color="#0995ff" type="submit" onClick={handleSubmit}>
+            submit(제출)
+          </Btn>
+        </Flex>
       </form>
-      <Btn color="red">clear(초기화)</Btn>
-      <Btn>submit(제출)</Btn>
-    </>
+    </main>
   );
 };
 
 export default AskMain;
 
 const Btn = styled.button`
-  background-color: ${(props) => (props.color ? props.color : '#0995ff')};
+  background-color: ${(props) => props.color};
   color: rgb(255, 255, 255);
   padding: 0.5rem;
   border: none;
   border-radius: 3px;
-  margin-top: ${(props) => (props.top ? props.top : '0.85rem')};
+  margin: 10px 20px;
   font-weight: bolder;
-`;
-
-const Container = styled.div`
-  width: 100%;
-`;
-
-const RowBox = styled.div`
-  width: 100%;
-  display: flex;
-`;
-
-const Viewer = styled.div`
-  width: calc(50% - 2rem);
-  height: 20rem;
-  padding: 1.2rem;
-  margin-top: 1.4rem;
-  border: 0.2rem solid gray;
 `;
 
 const Box = styled.div`
@@ -70,4 +97,10 @@ const Box = styled.div`
     padding: 1rem;
     border: 0.05rem solid black;
   }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props.direction};
+  justify-content: center;
 `;
