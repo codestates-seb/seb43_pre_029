ed0140521babb0077c81b0183c2e10c1fa4b51a0
Kadesti <adesti3527@gmail.com>
2023-04-23 22:28:53 +0900

    💬 Comment : 로그인 회원가입 로그인 추가

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,8 +4,9 @@ import { StackOverFlow } from '../components/Import-Logo';
 import EntireStyle from '../components/style/EntireStyle';
 import { LoginStyle, MainLogoStyle } from '../components/style/LoginStyle';
 
-import ModalSideHeader from '../components/Header/ModalSideHeader';
+import ModalSideHeader from '../components/Header/Modal/ModalSideHeader';
 
+/** 2023/03/18 - 로그인/회원가입 로고 - fe-hyungUk */
 const MainLogoContainer = () => {
   return (
     <MainLogoStyle>
@@ -14,6 +15,7 @@ const MainLogoContainer = () => {
   );
 };
 
+/** 2023/03/17 - 로그인 페이지 - fe-hyungUk */
 const Login = () => {
   return (
     <EntireStyle>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import EntireStyle from '../components/style/EntireStyle';
 import { SignUpStyle } from '../components/style/SignUpStyle';
 import OAuthButton from '../components/Login/OAuthButton';
 import SignUpForm from '../components/SignUp/SignUpForm';
 import SignUpInfo from '../images/SignUp.png';
 
-import ModalSideHeader from '../components/Header/ModalSideHeader';
+import ModalSideHeader from '../components/Header/Modal/ModalSideHeader';
 
 function SignUp() {
   return (
