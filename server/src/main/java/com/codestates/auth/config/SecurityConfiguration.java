c18de73a5cc63a159d998fcf46e5d94d82ee0e28
sjh4172 <hoon41272@gmail.com>
2023-04-24 09:43:32 +0900

    test

--- server/src/main/java/com/codestates/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/codestates/auth/config/SecurityConfiguration.java
@@ -2,13 +2,18 @@ package com.codestates.auth.config;
 
 import com.codestates.auth.filter.JwtAuthenticationFilter;
 import com.codestates.auth.jwt.JwtTokenizer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -20,6 +25,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
+	@Value("${spring.security.oauth2.client.registration.google.clientId}")
+	private String clientId;
+
+	@Value("${spring.security.oauth2.client.registration.google.clientSecret}")
+	private String clientSecret;
 	private final JwtTokenizer jwtTokenizer;
 
 	public SecurityConfiguration(JwtTokenizer jwtTokenizer) {
@@ -39,7 +49,11 @@ public class SecurityConfiguration {
 						.and()
 						.authorizeHttpRequests(authorize -> authorize
 										.anyRequest().permitAll()		// 모든 HTTP request 요청에 대해 접근 허용
-						);
+						)
+//						.authorizeHttpRequests(authorize -> authorize
+//										.anyRequest().authenticated()
+//						)
+						.oauth2Login(withDefaults());	// oauth 인증 활성화
 		return http.build();
 	}
 
@@ -71,5 +85,24 @@ public class SecurityConfiguration {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);	 // CORS 정책을 적용
 		return source;
+	}
+
+	// ClientRegistration 을 저장하기 위한 Repository
+	@Bean
+	public ClientRegistrationRepository clientRegistrationRepository() {
+		var clientRegistration = clientRegistration();
+
+		return new InMemoryClientRegistrationRepository(clientRegistration);   // ClientRegistration을 메모리에 저장
+	}
+
+	// ClientRegistration 인스턴스를 생성하는 private 메서드
+	private ClientRegistration clientRegistration() {
+		// Builder 패턴을 이용해 ClientRegistration 인스턴스를 제공
+		return CommonOAuth2Provider
+						.GOOGLE
+						.getBuilder("google")
+						.clientId(clientId)
+						.clientSecret(clientSecret)
+						.build();
 	}
 }
