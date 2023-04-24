5d22eae0b222888a7156a9f4b151f1354bb81ee1
sjh4172 <hoon41272@gmail.com>
2023-04-24 16:50:21 +0900

    :sparkles:Feat: test 완료

--- server/src/main/java/com/codestates/member/dto/MemberDto.java
+++ server/src/main/java/com/codestates/member/dto/MemberDto.java
@@ -48,7 +48,6 @@ public class MemberDto {
     @Getter
     @Setter
     @NoArgsConstructor
-<<<<<<< HEAD
     public static class CheckEmail {
         private String email;
     }
@@ -60,10 +59,9 @@ public class MemberDto {
     }
 
     @Getter
+    @Setter
     @NoArgsConstructor
-=======
     @AllArgsConstructor
->>>>>>> e5e4b56622c8dc0224a23302c72d15e4012f3bc9
     public static class MemberResponse{
 
         private String email;
