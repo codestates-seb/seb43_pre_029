444823ad7df524d119dd9a837e9e04c8a0b32207
dongwook98 <ehddnr8563@gmail.com>
2023-04-26 13:30:13 +0900

    ✨ Feat : 답글 삭제, 수정, 답글에 대한 댓글 UI구현

--- client/src/components/QInfo/Answer.jsx
+++ client/src/components/QInfo/Answer.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import AnswerSide from './AnswerSide';
+import { GoTriangleUp, GoTriangleDown } from 'react-icons/go';
+import { BsBookmark } from 'react-icons/bs';
+import { RxCountdownTimer } from 'react-icons/rx';
 import ReactQuill from 'react-quill';
 
 const AnswerMain = styled.div`
@@ -19,7 +23,24 @@ const AnswerMain = styled.div`
     }
   }
 `;
-
+const AswSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50px;
+  align-items: center;
+  justify-content: center;
+  margin: 3px;
+  button,
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 36px;
+    height: 36px;
+    margin: 3px;
+    padding: 0px;
+  }
+`;
 const ProfilLine = styled.div`
   margin-top: 20px;
   display: flex;
@@ -55,12 +76,23 @@ const User = styled.div`
   }
 `;
 
-const Answer = ({ answer }) => {
-  const { value, username, date, pic } = answer;
+const Answer = ({ answer, setAnswers, answers }) => {
+  const { value, username, date, pic, check } = answers;
+  const { id } = useParams();
+  const [commentModal, setCommentModal] = useState(false);
+  const [answerModal, setAnswerModal] = useState(false);
 
   return (
     <AnswerMain>
-      <AnswerSide />
+      {/* <AnswerSide answer={answer} check={check} /> */}
+      <AswSide>
+        <GoTriangleUp size="40px" color="lightgrey" className="flex-item" />
+        <div>0</div>
+        <GoTriangleDown size="40px" color="lightgrey" className="flex-item" />
+        <BsBookmark size="20px" color="lightgrey" className="flex-item" />
+        <RxCountdownTimer size="20px" color="lightgrey" className="flex-item" />
+        <button>체크</button>
+      </AswSide>
       <div className="answerMain">
         <ReactQuill
           value={value}
@@ -71,20 +103,97 @@ const Answer = ({ answer }) => {
         />
         <ProfilLine>
           <Profil>
-            <div className="date">{date}</div>
+            <div className="date">{answer.date}</div>
             <User>
               <div className="pic">
-                <img src={pic} alt="profile" />
+                <img src={answer.pic} alt="profile" />
               </div>
               <div>
-                <a href={'/'}>{username}</a>
+                <a href={'/'}>{answer.username}</a>
               </div>
             </User>
           </Profil>
         </ProfilLine>
-        <div className="addComment">Add a comment</div>
+        <Flex>
+          <Btn
+            color="blue"
+            onClick={() => {
+              setAnswerModal((prev) => !prev);
+            }}
+          >
+            답글 수정
+          </Btn>
+          {answerModal ? (
+            <FlexColumn>
+              <ReactQuill />
+            </FlexColumn>
+          ) : null}
+          <Btn
+            color="red"
+            onClick={() => {
+              axios.delete(`http://localhost:4000/question/${id}`);
+            }}
+          >
+            답글 삭제
+          </Btn>
+        </Flex>
+        <Flex>
+          <Btn
+            color="green"
+            onClick={() => {
+              setCommentModal(!commentModal);
+            }}
+          >
+            Add a comment
+          </Btn>
+        </Flex>
+        {commentModal ? (
+          <div>
+            <CommentInputStyle>
+              <form>
+                <input type="text" placeholder="댓글을 적어주세요." />
+                <button type="submit">확인</button>
+              </form>
+            </CommentInputStyle>
+          </div>
+        ) : null}
       </div>
     </AnswerMain>
   );
 };
 export default Answer;
+
+const Btn = styled.button`
+  display: block;
+  background-color: ${(props) => props.color};
+  color: rgb(255, 255, 255);
+  padding: 0.5rem;
+  border: none;
+  border-radius: 3px;
+  margin: 5px 5px;
+  font-weight: bolder;
+  cursor: pointer;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: end;
+`;
+
+const CommentInputStyle = styled.div`
+  input {
+    width: 85%;
+    border: 2px solid lightgray;
+    padding: 6px;
+  }
+  button {
+    width: 12%;
+    padding: 5px;
+    border: 2px solid lightgray;
+  }
+`;
+
+const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
