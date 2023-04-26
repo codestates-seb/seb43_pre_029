f7c81a57cc9c3ce2db478a677e32ee64be676f51
dongwook98 <ehddnr8563@gmail.com>
2023-04-26 13:32:12 +0900

    🎨 Style : 아이콘 추가

--- client/src/components/QInfo/QInfoSide.jsx
+++ client/src/components/QInfo/QInfoSide.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { GoTriangleUp, GoTriangleDown } from 'react-icons/go';
+import { BsBookmark } from 'react-icons/bs';
+import { RxCountdownTimer } from 'react-icons/rx';
 
 const InfoSide = styled.div`
   display: flex;
   flex-direction: column;
   width: 50px;
+  align-items: center;
+  justify-content: center;
+  .flex-item {
+    margin-top: 3px;
+  }
   button,
   div {
     display: flex;
@@ -21,11 +29,11 @@ const QInfoSide = () => {
   return (
     <div>
       <InfoSide>
-        <button>UP</button>
+        <GoTriangleUp size="40px" color="lightgrey" className="flex-item" />
         <div>0</div>
-        <button>Down</button>
-        <button>북마크</button>
-        <button>아이콘</button>
+        <GoTriangleDown size="40px" color="lightgrey" className="flex-item" />
+        <BsBookmark size="30px" color="lightgrey" className="flex-item" />
+        <RxCountdownTimer size="30px" color="lightgrey" className="flex-item" />
       </InfoSide>
     </div>
   );
