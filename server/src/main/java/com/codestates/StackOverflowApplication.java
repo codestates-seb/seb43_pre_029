c5670521aea607b6a0447c727476383f43ea3880
SEB_BE_43_손정훈 <84003339+sjh4172@users.noreply.github.com>
2023-04-26 18:16:02 +0900

    Revert "✨ Feat : 검색기능 추가"

--- server/src/main/java/com/codestates/StackOverflowApplication.java
+++ server/src/main/java/com/codestates/StackOverflowApplication.java
@@ -2,16 +2,8 @@ package com.codestates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EntityScan(
-		basePackageClasses = {Jsr310JpaConverters.class},
-		basePackages = {"com.codestates"}
-)
-@EnableJpaAuditing
 public class StackOverflowApplication {
 
 	public static void main(String[] args) {
--- server/src/main/java/com/codestates/answer/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codestates.answer.repository;
-
-import com.codestates.answer.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer,Long> {
-}
